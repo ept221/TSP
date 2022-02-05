6461e79f44f8283ed977fcef275b470a897bb82c
ept221 <ezra150@gmail.com>
2022-02-04 21:22:01 -0500

    refactored

--- sa.py
+++ sa.py
@@ -3,24 +3,27 @@ import random
 import math
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+##########################################################################
+# city class
+class City:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
 
 ##########################################################################
 # solver class
 class Solver:
-	def __init__(self):
-		self.cities_x_list = []
-		self.cities_y_list = []
+	def __init__(self, cities, iterations):
 
-		self.solution_x_list = []
-		self.solution_y_list = []
+		self.cities = cities
+
+		self.solution = []
 		self.solution_cost = 0
 
-		self.temp_x_list = []
-		self.temp_y_list = []
+		self.temp = []
 		self.temp_cost = 0
 
-		self.best_x_list = []
-		self.best_y_list = []
+		self.best = []
 		self.best_cost = 0
 
 		self.num_of_swaps = 1
@@ -29,93 +32,94 @@ class Solver:
 		self.T = 0
 		self.k = 1
 
-	def plot_cities(self, ax):
-		colors = [np.random.rand() for i in range(len(self.cities_x_list))]
-		ax.scatter(self.cities_x_list, self.cities_y_list, s=100, c=colors, alpha = 0.8)
+		self.iterations = iterations
 
-	def plot_path(self, ax):
-		self.path_plot, = ax.plot(self.solution_x_list,self.solution_y_list)
+		self.fig, self.ax = plt.subplots()
 
-	def compute_cost(self,x_list,y_list):
+		self.create_path()
+		self.plot_cities()
+		self.plot_path()
+
+	def plot_cities(self):
+		cities_x_list = [city.x for city in self.cities]
+		cities_y_list = [city.y for city in self.cities]
+		colors = [np.random.rand() for i in range(len(self.cities))]
+		self.ax.scatter(cities_x_list, cities_y_list, s=100, c=colors, alpha = 0.8)
+
+	def plot_path(self):
+		solution_x_list = [city.x for city in self.solution]
+		solution_y_list = [city.y for city in self.solution]
+		self.path_plot, = self.ax.plot(solution_x_list,solution_y_list)
+
+	def compute_cost(self, path):
 		cost = 0
-		for i in range(len(x_list)-1):
-			dx = x_list[i+1] - x_list[i]
-			dy = y_list[i+1] - y_list[i]
+		for i in range(len(path)-1):
+			dx = path[i+1].x - path[i].x
+			dy = path[i+1].y - path[i].y
 			cost += (dx**2 + dy**2)**0.5
 		return cost
 
 	def compute_solution_cost(self):
-		self.solution_cost = self.compute_cost(self.solution_x_list,self.solution_y_list)
+		self.solution_cost = self.compute_cost(self.solution)
 
 	def compute_temp_cost(self):
-		self.temp_cost = self.compute_cost(self.temp_x_list,self.temp_y_list)
+		self.temp_cost = self.compute_cost(self.temp)
 
 	def create_path(self):
-		self.solution_x_list = [*self.cities_x_list, self.cities_x_list[0]]
-		self.solution_y_list = [*self.cities_y_list, self.cities_y_list[0]]
+		self.solution = [*self.cities, self.cities[0]]
 		self.compute_solution_cost()
-
-		self.best_x_list = self.solution_x_list
-		self.best_y_list = self.solution_y_list
+		self.best = self.solution
 		self.best_cost = self.solution_cost
 
 	def swap(self):
-		temp = list(zip(self.solution_x_list,self.solution_y_list))
+		self.temp = self.solution.copy()
 		for i in range(self.num_of_swaps):
-			idx = range(len(temp)-1)
+			idx = range(len(self.temp)-1)
 			i1,i2 = random.sample(idx,2)
-			temp[i1], temp[i2] = temp[i2], temp[i1]
-		temp[-1] = temp[0]
-		self.temp_x_list, self.temp_y_list = zip(*temp)
-
+			self.temp[i1], self.temp[i2] = self.temp[i2], self.temp[i1]
+		self.temp[-1] = self.temp[0]
 
 	def select(self,frame):
 		self.T = frame*self.k + 0.000000000000000001
 		if(self.temp_cost < self.solution_cost or np.random.rand() > math.exp((self.solution_cost-self.temp_cost)/self.T)):
-			self.solution_x_list = self.temp_x_list
-			self.solution_y_list = self.temp_y_list
+			self.solution = self.temp
 			self.solution_cost = self.temp_cost
+		else:
+			return
 
 		if(self.solution_cost < self.best_cost):
-			self.best_x_list = self.solution_x_list
-			self.best_y_list = self.solution_y_list
+			self.best = self.solution
 			self.best_cost = self.solution_cost
 
 	def update_path_plot(self):
-		self.path_plot.set_data(self.solution_x_list,self.solution_y_list)
+		solution_x_list = [city.x for city in self.solution]
+		solution_y_list = [city.y for city in self.solution]
+		self.path_plot.set_data(solution_x_list,solution_y_list)
 		print("current cost: \t" + str(self.solution_cost))
 
 	def plot_best_path(self):
-		self.path_plot.set_data(self.best_x_list,self.best_y_list)
+		best_x_list = [city.x for city in self.best]
+		best_y_list = [city.y for city in self.best]
+		self.path_plot.set_data(best_x_list,best_y_list)
 		print("best cost: \t" + str(self.best_cost))
+
+	def update(self, frame, max_frames):
+		if(frame < (max_frames-1)):
+			self.swap()
+			self.compute_temp_cost()
+			self.select(frame)
+			self.update_path_plot()
+		else:
+			self.plot_best_path()
+
+	def animate(self):
+		ani = animation.FuncAnimation(self.fig, self.update, frames=self.iterations, interval=1, blit=False, repeat=False, fargs=(self.iterations,))
+		plt.show()
 ##########################################################################
 # create solver and initial list of cities
-num_of_cities = 20
-iterations = 3000
-solver = Solver()
-solver.cities_x_list = [np.random.rand() for i in range(num_of_cities)]
-solver.cities_y_list = [np.random.rand() for i in range(num_of_cities)]
-##########################################################################
-# create the initial plot
-fig, ax = plt.subplots()
-solver.create_path()
-solver.plot_cities(ax)
-solver.plot_path(ax)
-##########################################################################
-# animation update function
-def update(frame, max_frames, solver):
-	if(frame < (max_frames-1)):
-		solver.swap()
-		solver.compute_temp_cost()
-		solver.select(frame)
-		solver.update_path_plot()
-	else:
-		solver.plot_best_path()
+num_of_cities = 14
+cities = [City(np.random.rand(),np.random.rand()) for i in range(num_of_cities)]
 
-	return
-
-##########################################################################
-# animate
-ani = animation.FuncAnimation(fig, update, frames=iterations, interval=1, blit=False, repeat=False, fargs=(iterations,solver))
-plt.show()
+solver = Solver(cities,1000)
+solver.animate()
 ##########################################################################
